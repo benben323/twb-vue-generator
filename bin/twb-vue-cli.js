291d0271a1b550914229075d83888a04e457c8e9
汤稳兵 <535371526@qq.com>
2017-12-07 14:25:37 +0800

    udpate

--- bin/twb-vue-cli.js
+++ bin/twb-vue-cli.js
@@ -1,15 +1,252 @@
 #!/usr/bin/env node
 
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var sortedObject = require('sorted-object')
+var path = require('path')
 var program = require('commander')
+var readline = require('readline')
+var util = require('util')
+
+var MODE_0666 = parseInt('0666', 8)
+var MODE_0755 = parseInt('0755', 8)
+
+var _exit = process.exit
+var pkg = require('../package.json')
+
+var version = pkg.version
+
+process.exit = exit
 
 program
-  .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .name('twbvue')
+  .version(version, '   --version')
+  .usage('[options] [dir]')
+  .option('-v, --view <engine>', 'add view <engine> support (dust|ejs|jade) (defaults to jade)')
+  .option('    --git', 'add .gitignore')
+  .option('-f, --force', 'force on non-empty directory')
   .parse(process.argv)
 
-console.log('you ordered a pizza with:')
-if (program.peppers) console.log(' - peppers')
-if (program.bbqSauce) console.log(' - bbq')
-console.log(' - %s cheese', program.cheese)
+if (!exit.exited) {
+  main()
+}
+/**
+ * Install an around function; AOP.
+ */
+
+function around (obj, method, fn) {
+  var old = obj[method]
+
+  obj[method] = function () {
+    var args = new Array(arguments.length)
+    for (var i = 0; i < args.length; i++) args[i] = arguments[i]
+    return fn.call(this, old, args)
+  }
+}
+
+/**
+ * Install a before function; AOP.
+ */
+
+function before (obj, method, fn) {
+  var old = obj[method]
+
+  obj[method] = function () {
+    fn.call(this)
+    old.apply(this, arguments)
+  }
+}
+
+/**
+ * Prompt for confirmation on STDOUT/STDIN
+ */
+
+function confirm (msg, callback) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question(msg, function (input) {
+    rl.close()
+    callback(/^y|yes|ok|true$/i.test(input))
+  })
+}
+
+/**
+ * Copy file from template directory.
+ */
+
+function copyTemplate (from, to) {
+  from = path.join(__dirname, '..', 'templates', from)
+  write(to, fs.readFileSync(from, 'utf-8'))
+}
+
+function createApplication (name, path) {
+  var wait = 5
+
+  console.log()
+  function complete () {
+    if (--wait) return
+    var prompt = launchedFromCmd() ? '>' : '$'
+
+    console.log()
+    console.log('   install dependencies:')
+    console.log('     %s cd %s && npm install', promp, path)
+    console.log()
+    console.log('   run the app:')
+
+    if (launchedFromCmd()) {
+      console.log('   %s SET DEBUG=%s:* & npm start', prompt, name)
+    } else {
+      console.log('   %s DEBUG=%s:* npm start', prompt, name)
+    }
+
+    console.log()
+  }
+
+  mkdir(path, function () {
+    
+  })
+}
+
+/**
+ * Check if the given directory `path` is empty.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ */
+
+function emptyDirectory (path, fn) {
+  fs.readdir(path, function (err, files) {
+    if (err && err.code !== 'ENOENT') throw err
+    fn(!files || !files.length)
+  })
+}
+
+/**
+ * Graceful exit for async STDIO
+ */
+
+function exit (code) {
+  // flush output for Node.js Windows pipe bug
+  // https://github.com/joyent/node/issues/6247 is just one bug example
+  // https://github.com/visionmedia/mocha/issues/333 has a good discussion
+  function done () {
+    if (!(draining--)) _exit(code)
+  }
+
+  var draining = 0
+  var streams = [process.stdout, process.stderr]
+
+  exit.exited = true
+
+  streams.forEach(function (stream) {
+    // submit empty write request and wait for completion
+    draining += 1
+    stream.write('', done)
+  })
+
+  done()
+}
+
+/**
+ * Determine if launched from cmd.exe
+ */
+
+function launchedFromCmd () {
+  return process.platform === 'win32' &&
+    process.env._ === undefined
+}
+
+/**
+ * Create an app name from a directory path, fitting npm naming requirements.
+ *
+ * @param {String} pathName
+ */
+
+function createAppName (pathName) {
+  return path.basename(pathName)
+    .replace(/[^A-Za-z0-9.()!~*'-]+/g, '-')
+    .replace(/^[-_.]+|-+$/g, '')
+    .toLowerCase()
+}
+
+function main () {
+  var destinationPath = program.args.shift() || '.'
+
+  var appName = createAppName(path.resolve(destinationPath)) || 'hello-world'
+  
+  // TODO  参数校检
+
+  emptyDirectory(destinationPath, function (empty) {
+    if (empty || program.force) {
+      createApplication(appName, destinationPath)
+    } else {
+      confirm('destination is not empty, continue? [y/N] ', function (ok) {
+        if (ok) {
+         process.stdin.destroy()
+         createApplication(appName, destinationPath)
+        } else {
+          console.error('aborting')
+          exit(1)
+        }
+      })
+    }
+  })
+}
+
+/**
+ * Mkdir -p.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ */
+
+function mkdir (path, fn) {
+  mkdirp(path, MODE_0755, function (err) {
+    if (err) throw err
+    console.log('   \x1b[36mcreate\x1b[0m : ' + path)
+    fn && fn()
+  })
+}
+
+/**
+ * Generate a callback function for commander to warn about renamed option.
+ *
+ * @param {String} originalName
+ * @param {String} newName
+ */
+
+function renamedOption (originalName, newName) {
+  return function (val) {
+    warning(util.format("option `%s' has been renamed to `%s'", originalName, newName))
+    return val
+  }
+}
+
+/**
+ * Display a warning similar to how errors are displayed by commander.
+ *
+ * @param {String} message
+ */
+
+function warning (message) {
+  console.error()
+  message.split('\n').forEach(function (line) {
+    console.error('  warning: %s', line)
+  })
+  console.error()
+}
+
+/**
+ * echo str > path.
+ *
+ * @param {String} path
+ * @param {String} str
+ */
+
+function write (path, str, mode) {
+  fs.writeFileSync(path, str, { mode: mode || MODE_0666 })
+  console.log('   \x1b[36mcreate\x1b[0m : ' + path)
+}
